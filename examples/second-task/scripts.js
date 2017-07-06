a8bddcb64cf0f3072551d7828e7e692115131647
Vyacheslav Yefremov <efremovslavik2000@gmail.com>
2017-07-06 21:05:55 +0300

    in progress

--- examples/second-task/scripts.js
+++ examples/second-task/scripts.js
@@ -1,6 +1,18 @@
 (function(document){
     var table = document.getElementById("table");
     var rows = table.getElementsByTagName("tr");
+    var elements = [];
+    var sum = [];
 
-    console.log(rows, table);
+    for(var i = 0; i < rows.length; i++) {
+        if(elements.length <= i) {
+            elements.push(rows[i]);
+
+            for(var a = 0; a < rows.length; a++) {
+                elements[i] += rows[i].getElementsByTagName("td");
+            }
+        }
+        console.log(elements[i]);
+    }
+
 })(document);
