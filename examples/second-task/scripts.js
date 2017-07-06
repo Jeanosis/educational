20747bc5aa211a346b855005188080790d3bc684
Jeanosis <plaits123@gmail.com>
2017-07-06 21:58:38 +0300

    algorithm finished

--- examples/second-task/scripts.js
+++ examples/second-task/scripts.js
@@ -1,18 +1,25 @@
-(function(document){
+(function(document, window){
     var table = document.getElementById("table");
     var rows = table.getElementsByTagName("tr");
-    var elements = [];
     var sum = [];
 
     for(var i = 0; i < rows.length; i++) {
-        if(elements.length <= i) {
-            elements.push(rows[i]);
+        var columns = rows[i].getElementsByTagName('td');
 
-            for(var a = 0; a < rows.length; a++) {
-                elements[i] += rows[i].getElementsByTagName("td");
+        for (var j = 0; j < columns.length; j++) {
+            var cell = parseFloat(columns[j].innerHTML);
+
+            if (isNaN(cell)) {
+                continue;
+            }
+            
+            if (j >= sum.length) {
+                sum.push(cell);
+            } else {
+                sum[j] += cell;
             }
         }
-        console.log(elements[i]);
     }
 
-})(document);
+    console.log('SUM', sum);
+})(document, window);
